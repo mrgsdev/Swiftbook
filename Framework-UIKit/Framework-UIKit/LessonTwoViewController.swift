4c243a6ddc07c3239ab107000af7da287efacd05
mrgsdev <mrgsdev@gmail.com>
2024-02-19 11:16:36 +0300

    UISlider

--- Framework-UIKit/Framework-UIKit/LessonTwoViewController.swift
+++ Framework-UIKit/Framework-UIKit/LessonTwoViewController.swift
@@ -9,16 +9,34 @@ import UIKit
 
 class LessonTwoViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.isHidden = !label.isHidden
+      
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.value = 50
+        slider.minimumTrackTintColor = .black
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+//        label.isHidden = !label.isHidden
+        label.text = String(Int(slider.value))
         label.font = label.font.withSize(35)
         label.textAlignment = .center
         label.numberOfLines = 2
         
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        
+        
+    }
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String(Int(sender.value))
+        
+ 
+        self.view.backgroundColor = self.view.backgroundColor!.withAlphaComponent(CGFloat(sender.value))
         
     }
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
