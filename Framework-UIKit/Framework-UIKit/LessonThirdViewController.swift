fd510bdda725e6ebdaa0f69da580e01233089c90
mrgsdev <mrgsdev@gmail.com>
2024-02-19 14:22:15 +0300

    UIStepper & UIActivityIndicatorView & UIProgressView & UIProgressView

--- Framework-UIKit/Framework-UIKit/LessonThirdViewController.swift
+++ Framework-UIKit/Framework-UIKit/LessonThirdViewController.swift
@@ -3,22 +3,69 @@
 //  Framework-UIKit
 //
 //  Created by mrgsdev on 19.02.2024.
-//  UITextView
+//  UITextView & UIStepper & UIActivityIndicatorView & UIProgressView & UIProgressView
 
 import UIKit
 
 class LessonThirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        textView.text = nil
+        
+        progressView.setProgress(0, animated: true)
+        
+        stepper.value = 17
+        stepper.minimumValue = 10
+        stepper.maximumValue = 25
+        stepper.tintColor = .white
+        stepper.backgroundColor = .gray
+        stepper.layer.cornerRadius = 5
+        
+        textView.isHidden = true
+//        textView.alpha = 0
+        stepper.isHidden = true
+        countLabel.isHidden = true
+        
         textView.delegate = self
         textView.font = UIFont(name: "HelveticaNeue", size: 16)
         textView.layer.cornerRadius = 10
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-           NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+//        UIView.animate(withDuration: 0, delay: 3, options: .curveLinear) { //UIActivityIndicatorView
+//            self.textView.alpha = 1
+//        } completion: { finished in
+//            self.activityIndicator.stopAnimating()
+//            self.textView.isHidden = false
+//            self.stepper.isHidden = false
+//            self.countLabel.isHidden = false
+//        }
+        
+        
+        activityIndicator.hidesWhenStopped = true // индикатор по окончанию уйдет
+        activityIndicator.color = .red
+        activityIndicator.startAnimating()
+        
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+            if self.progressView.progress != 1 {
+                self.progressView.progress += 0.2
+            }else{
+                self.activityIndicator.stopAnimating()
+                self.textView.isHidden = false
+                self.stepper.isHidden = false
+                self.countLabel.isHidden = false
+                self.progressView.isHidden = true
+           
+            }
+        }
+        
     }
     
     @objc func keyboardWillShow(_ notification: Notification) {
@@ -30,19 +77,26 @@ class LessonThirdViewController: UIViewController {
             textView.contentInset.bottom = bottomInset
         }
     }
-
+    
     @objc func keyboardWillHide(_ notification: Notification) {
         // Сбрасываем contentInset, когда клавиатура скрывается
         textView.contentInset = .zero
     }
-
-
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         self.view.endEditing(true)
-//      textView.resignFirstResponder()
+        //      textView.resignFirstResponder()
     }
-
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        if let font = textView.font?.fontName {
+            let fontSize = CGFloat(sender.value)
+            textView.font = UIFont(name: font, size: fontSize)
+        }
+        
+    }
 }
 
 
@@ -59,6 +113,7 @@ extension LessonThirdViewController:UITextViewDelegate{
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         countLabel.text = "\(textView.text.count)"
-        return textView.text.count + (text.count - range.length) <= 50
+        return textView.text.count + (text.count - range.length) <= 3000
     }
+    
 }
