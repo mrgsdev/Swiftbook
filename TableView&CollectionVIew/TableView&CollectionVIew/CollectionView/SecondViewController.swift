29162468753f288145dec40d15e7ac1e567f39e9
mrgsdev <mrgsdev@gmail.com>
2024-02-23 10:11:27 +0300

    Share Photos Action

--- TableView&CollectionVIew/TableView&CollectionVIew/CollectionView/SecondViewController.swift
+++ TableView&CollectionVIew/TableView&CollectionVIew/CollectionView/SecondViewController.swift
@@ -19,6 +19,17 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func shareAction(_ sender: Any) {
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("Успешно!")
+            }
+        }
+        
+        
+        present(shareController, animated: true, completion: nil)
+        
     }
 
 }
