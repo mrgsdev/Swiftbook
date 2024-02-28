29c21626bafe2ac471fbfc00c708dfd724df9cee
mrgsdev <mrgsdev@gmail.com>
2024-02-28 13:57:39 +0300

    Map View Controller

---
+++ MyPlaces/MyPlaces/Controller/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by mrgsdev on 28.02.2024.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeVC(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
