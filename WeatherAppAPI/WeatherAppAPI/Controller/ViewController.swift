c7388b054f6e97747d5a155f7dba3d098fe6aec6
mrgsdev <mrgsdev@gmail.com>
2024-03-03 11:46:52 +0300

    Setup App

---
+++ WeatherAppAPI/WeatherAppAPI/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WeatherAppAPI
+//
+//  Created by mrgsdev on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    let networkWeatherManager = NetworkWeatherManager()
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        networkWeatherManager.fetchCurrentWeather(forCity: "Moscow")
+    }
+
+
+}
+
---
+++ WeatherAppAPI/WeatherAppAPI/Model/NetworkWeatherManager.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkWeatherManager.swift
+//  WeatherAppAPI
+//
+//  Created by mrgsdev on 03.03.2024.
+//
+
+import Foundation
+let apiKey = "aa3f365e831f00e797050758578c8aa3"
+struct NetworkWeatherManager {
+    func fetchCurrentWeather(forCity city: String) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            }
+        }
+        task.resume()
+    }
+}
