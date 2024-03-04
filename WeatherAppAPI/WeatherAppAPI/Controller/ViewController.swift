27f6f073421e25178b4d1a6ab7c961746ff4c506
mrgsdev <mrgsdev@gmail.com>
2024-03-04 17:56:57 +0300

    Delegation

--- WeatherAppAPI/WeatherAppAPI/Controller/ViewController.swift
+++ WeatherAppAPI/WeatherAppAPI/Controller/ViewController.swift
@@ -25,12 +25,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkWeatherManager.onCompletion = { currentWeather in
-            print(currentWeather.cityName)
-        }
+        networkWeatherManager.delegate = self
         networkWeatherManager.fetchCurrentWeather(forCity: "Moscow")
     }
 
 
 }
 
+extension ViewController: NetworkWeatherManagerDelegate {
+    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
+        print(currentWeather.cityName)
+    }
+}
--- WeatherAppAPI/WeatherAppAPI/Model/NetworkWeatherManager.swift
+++ WeatherAppAPI/WeatherAppAPI/Model/NetworkWeatherManager.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
+
+protocol NetworkWeatherManagerDelegate: class {
+    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
+}
+
 let apiKey = "aa3f365e831f00e797050758578c8aa3" // a new key is generated in 45 minutes
 
 struct NetworkWeatherManager {
-    var onCompletion: ((CurrentWeather) -> Void)?
+    weak var delegate: NetworkWeatherManagerDelegate?
     func fetchCurrentWeather(forCity city: String) {
         let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
         guard let url = URL(string: urlString) else { return }
@@ -17,7 +22,7 @@ struct NetworkWeatherManager {
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 if let currentWeather = self.parseJSON(withData: data) {
-                    self.onCompletion?(currentWeather)
+                    self.delegate?.updateInterface(self, with: currentWeather)
                 }
             }
         }
