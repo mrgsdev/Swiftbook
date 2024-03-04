09afc6f94ee83a5b0e0957b1bbb255a9737e3e5c
mrgsdev <mrgsdev@gmail.com>
2024-03-04 17:52:30 +0300

    Передаем данные через клоужеры

--- WeatherAppAPI/WeatherAppAPI/Model/NetworkWeatherManager.swift
+++ WeatherAppAPI/WeatherAppAPI/Model/NetworkWeatherManager.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 let apiKey = "aa3f365e831f00e797050758578c8aa3" // a new key is generated in 45 minutes
+
 struct NetworkWeatherManager {
+    var onCompletion: ((CurrentWeather) -> Void)?
     func fetchCurrentWeather(forCity city: String) {
         let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                 parseJSON(withData: data)
+                if let currentWeather = self.parseJSON(withData: data) {
+                    self.onCompletion?(currentWeather)
+                }
             }
         }
         task.resume()
