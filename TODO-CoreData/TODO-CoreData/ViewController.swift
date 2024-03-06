b1c20b29ca74a000143ec61da94ba22960ae9f3c
mrgsdev <mrgsdev@gmail.com>
2024-03-06 09:48:18 +0300

    CoreData App

---
+++ TODO-CoreData/TODO-CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODO-CoreData
+//
+//  Created by mrgsdev on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
