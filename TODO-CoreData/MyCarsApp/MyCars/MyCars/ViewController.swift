ddbb6167ba9c59e28c24d43214b0fd82efe806fd
mrgsdev <mrgsdev@gmail.com>
2024-03-07 17:08:48 +0300

    Add second app

---
+++ TODO-CoreData/MyCarsApp/MyCars/MyCars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by mrgsdev on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
