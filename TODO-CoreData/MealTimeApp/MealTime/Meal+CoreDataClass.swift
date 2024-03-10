c5f4bef1a2d6ad570b1d4d5bc60c4434a6ff8600
mrgsdev <mrgsdev@gmail.com>
2024-03-10 10:28:36 +0300

    Add CoreData Model

---
+++ TODO-CoreData/MealTimeApp/MealTime/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by mrgsdev on 10.03.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ TODO-CoreData/MealTimeApp/MealTime/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by mrgsdev on 10.03.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
