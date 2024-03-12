1700e63de033f35e03ed6e49419b34aeca522c61
mrgsdev <mrgsdev@gmail.com>
2024-03-12 10:41:20 +0300

    JSONDecoder Start

---
+++ Networking/Image App/ImageApp/ImageApp/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  ImageApp
+//
+//  Created by mrgsdev on 12.03.2024.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessons: UILabel!
+    @IBOutlet var numberOfTests: UILabel!
+}
