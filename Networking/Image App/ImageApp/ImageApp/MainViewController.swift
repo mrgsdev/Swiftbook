b80503c2f248eaf96e6edc39e8aac38339ed6ee5
mrgsdev <mrgsdev@gmail.com>
2024-03-12 10:34:29 +0300

    POST Requests

--- Networking/Image App/ImageApp/ImageApp/MainViewController.swift
+++ Networking/Image App/ImageApp/ImageApp/MainViewController.swift
@@ -31,6 +31,31 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let userData = ["Course": "Networking", "Lesson": "GET and POST"]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            guard let response, let data else { return }
+            
+            print("response POST \(response)")
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print("json  POST \(json)")
+            } catch {
+                print("error POST \(error)")
+            }
+        } .resume()
     }
 
 }
