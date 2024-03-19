4e41e310e73cd252abf7884268ccdf7b7e6e65e2
mrgsdev <mrgsdev@gmail.com>
2024-03-19 14:23:48 +0300

    Add new project MVC

---
+++ MVC/MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by mrgsdev on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
