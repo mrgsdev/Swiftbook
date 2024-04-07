848169a348284118dd18d209693e9dd9f268aedc
mrgsdev <mrgsdev@gmail.com>
2024-04-07 12:02:07 +0300

    Add Second Project MVVM-2

---
+++ MVVM/MVVM-2/MVVM-2/MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by mrgsdev on 07.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
