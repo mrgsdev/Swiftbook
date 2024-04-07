1a89401f7967aa678ac5e26881c7892ac4ca0b0a
mrgsdev <mrgsdev@gmail.com>
2024-04-07 11:39:50 +0300

    MVC

---
+++ MVVM/MVVM-1/MVVM-1/MVVM-1/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-1
+//
+//  Created by mrgsdev on 07.04.2024.
+//
+
+import Foundation
+
+struct Profile{
+    var name:String
+    var secondName:String
+    var age:Int
+}
--- MVVM/MVVM-1/MVVM-1/MVVM-1/ViewController.swift
+++ MVVM/MVVM-1/MVVM-1/MVVM-1/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondName: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var profile: Profile? {
+        didSet{
+            guard let profile else {
+          return
+            }
+            self.nameLabel.text = profile.name
+            self.secondName.text = profile.secondName
+            self.ageLabel.text = "\(profile.age)"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        profile = Profile(name: "John", secondName: "Smith", age: 2)
+        
     }
 
 
